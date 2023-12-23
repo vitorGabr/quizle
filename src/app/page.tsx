b7c95ce10133b4481768a586d35f636a0187778f
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2023-12-22 23:32:54 -0300

    Update eslint configuration and format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import { Navbar } from '@/components/layout/navbar';
-import { Words } from '@/components/words';
-import { Stack } from 'styled-system/jsx';
-import { Keyboard } from '@/components/keyboard';
+import { Keyboard } from "@/components/keyboard";
+import { Navbar } from "@/components/layout/navbar";
+import { Words } from "@/components/words";
+import { Stack } from "styled-system/jsx";
 
 export default function Home() {
-  return (
-    <Stack
-      w="100%"
-      h="100dvh"
-      py="2"
-      justifyContent={['center']}
-      alignItems={['center']}
-      gap={'10'}
-      bgColor="bg.surface"
-    >
-      <Navbar />
-      <Words />
-      <Keyboard />
-    </Stack>
-  );
+	return (
+		<Stack
+			w="100%"
+			h="100dvh"
+			py="2"
+			justifyContent={["center"]}
+			alignItems={["center"]}
+			gap={"10"}
+			bgColor="bg.surface"
+		>
+			<Navbar />
+			<Words />
+			<Keyboard />
+		</Stack>
+	);
 }
