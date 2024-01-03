e418804893b7a317e660bd61c60c778f6641e33b
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-01-03 18:23:44 -0300

    Update middleware authorization check

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,8 @@ export const config = {
 
 export default async function middleware(request: NextRequest) {
   const authorization = request.headers.get("Authorization");
-  if (authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+
+  if (authorization !== process.env.AUTHORIZATION) {
     return Response.json(
       { success: false, message: "authentication failed" },
       { status: 401 }
